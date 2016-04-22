43e9889e3077d122d86437abda15fd9ef2c01d5e
btkunisch <btkunisch_@hotmail.com>
2016-04-22 21:15:14 +0000

    user_profile in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401223910) do
+ActiveRecord::Schema.define(version: 20160422194337) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
